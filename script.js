9f51b606f3a0f13005a1f62e2c0a6777a7994023
Kiran TJ <166934975+thewelshfiend@users.noreply.github.com>
2024-06-04 21:31:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+const texts = document.getElementsByTagName('strong');
+
 function highlight() {
     //Write your code here
-
-
+    for (let x of texts)
+    {
+        x.style.color = 'green';
+    }
 }
-
 
 function return_normal() {
     //Write your code here
-
-    
+    for (let x of texts)
+    {
+        x.style.color = '';
+    }
 }
